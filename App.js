6c355c66dadae4bdb3554e9437867b072bf47c5a
f020202 <111269736+f020202@users.noreply.github.com>
2024-10-24 16:07:41 +0900

    design: MainScreen.js 및 Hall 온보딩

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { TouchableOpacity, Image, Text } from 'react-native';
+// import colors from '../../config/colors';
+// import { fonts } from '../../config/fonts';
+
 import MainScreen from './src/screens/MainScreen';
 import LikeScreen from './src/screens/LikeScreen';
 import MypageScreen from './src/screens/MypageScreen';
 import HallPriceScreen from './src/screens/HallPriceScreen';  // 새로운 화면 (탭 없는 화면)
 import HallListScreen from './src/screens/HallListScreen';  // 새로운 화면 (탭 없는 화면)
+import HallTagsScreen from './src/screens/HallTagsScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -14,29 +19,29 @@ const Stack = createStackNavigator();
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen 
-        name="Likes" 
+      <Tab.Screen
+        name="Likes"
         component={LikeScreen}
-        options={{ 
+        options={{
           tabBarLabel: '찜',
           headerShown: false
-        }}  
+        }}
       />
       <Tab.Screen
         name="Main"
         component={MainScreen}
-        options={{ 
+        options={{
           tabBarLabel: '홈',
           headerShown: false
-        }}  
+        }}
       />
-      <Tab.Screen 
-        name="My Page" 
+      <Tab.Screen
+        name="My Page"
         component={MypageScreen}
-        options={{ 
+        options={{
           tabBarLabel: '마이페이지',
           headerShown: false
-        }} 
+        }}
       />
     </Tab.Navigator>
   );
@@ -52,17 +57,31 @@ export default function App() {
           component={TabNavigator}
           options={{ headerShown: false }}  // 탭 네비게이션의 상단바 숨김
         />
-        
+
         {/* 바텀탭 없이 표시될 새로운 화면 */}
-        <Stack.Screen 
-          name="HallPrice" 
+        <Stack.Screen
+          name="HallPrice"
           component={HallPriceScreen}  // 새 화면을 위한 컴포넌트
           options={{ headerShown: true }}  // 필요에 따라 상단바를 숨기거나 보이게 설정 가능
         />
 
-          {/* 바텀탭 없이 표시될 새로운 화면 */}
-          <Stack.Screen 
-          name="HallList" 
+        <Stack.Screen
+          name="HallTags"
+          component={HallTagsScreen}  // 새 화면을 위한 컴포넌트
+          options={({ navigation }) => ({
+            headerTitle: '웨딩홀 알아보기',
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image source={require('./assets/left.png')} style={{ height: 24, width: 24, marginLeft:10, }} />
+              </TouchableOpacity>
+            ),
+          })} // 필요에 따라 상단바를 숨기거나 보이게 설정 가능
+        />
+
+
+        {/* 바텀탭 없이 표시될 새로운 화면 */}
+        <Stack.Screen
+          name="HallList"
           component={HallListScreen}  // 새 화면을 위한 컴포넌트
           options={{ headerShown: true }}  // 필요에 따라 상단바를 숨기거나 보이게 설정 가능
         />
--- src/components/CustomButtonSkip.js
+++ src/components/CustomButtonSkip.js
@@ -1,8 +1,7 @@
-// CustomButton.js
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function CustomButton({ title, onPress }) {
+export default function CustomButtonSkip({ title, onPress }) {
   return (
     <TouchableOpacity style={styles.button} onPress={onPress}>
       <Text style={styles.buttonText}>{title}</Text>
@@ -12,13 +11,14 @@ export default function CustomButton({ title, onPress }) {
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#222222',
+    backgroundColor: '#ECECEC',
     width:'100%',
     height:45,
-    justifyContent:'center',
     padding: 10,
     borderRadius: 7,
     alignItems: 'center',
+    justifyContent:'center',
+    marginVertical:10,
   },
   buttonText: {
     color: '#fff',
